a27a2011b91586978afcd5432f1761019b42d980
mungiyo <wkdtjdxo1234@naver.com>
2021-08-04 17:41:03 +0900

    update

--- webproject_1/main/index.py
+++ webproject_1/main/index.py
@@ -8,8 +8,8 @@ import os
 
 app = Flask(__name__)
 
-uploaded_file_path = os.getcwd() + "/webproject_1/main/static/uploads/"
-gened_file_path = os.getcwd() + "/webproject_1/main/static/gen_files/"
+upload_path = os.getcwd() + "/webproject_1/main/static/files/upload_files/"
+gen_path = os.getcwd() + "/webproject_1/main/static/files/gen_files/"
 
 # file_path에 있는 파일들 삭제하는 함수
 def files_removing(file_path):
@@ -30,28 +30,54 @@ def index():
 def order():
     return render_template('order.html')
 
-@app.route('/complete', methods = ['GET', 'POST'])
-def transform_file():
+@app.route('/order/complete', methods = ['GET', 'POST'])
+def order_transform():
     if request.method == 'POST':
-        files_removing(uploaded_file_path)
-        files_removing(gened_file_path)
+        # 경로에 파일이 있으면 그 파일들 삭제
+        files_removing(upload_path)
+        files_removing(gen_path)
         try:
             f = request.files['file']
-            upload_file_path = uploaded_file_path + secure_filename(f.filename)
-
-            gen_file_path_1 = gened_file_path + '발주파일.xlsx'
-            gen_file_path_2 = gened_file_path + '물류파일.xlsx'   # 물류파일 저장할 경로
-            f.save(upload_file_path)
-
-            order2order(upload_file_path).to_excel(gen_file_path_1)
-            distributon_from_orderinfo(upload_file_path).to_excel(gen_file_path_2)
-            
-            excel2json(gen_file_path_1, gened_file_path + "발주파일.json")
-            excel2json(gen_file_path_2, gened_file_path + "물류파일.json")
-            
-            return render_template('complete.html')
+            upload_file = upload_path + secure_filename(f.filename)
+            gen_file = gen_path + '발주파일.xlsx'
+            f.save(upload_file)
+            order2order(upload_file).to_excel(gen_file)
+            excel2json(gen_file, gen_path + "발주파일.json")
+            return render_template('order_complete.html')
         except:
             return '파일 변환에 실패하였습니다. 다시 시도해주세요.'
+
+@app.route('/distribution', methods = ['GET', 'POST'])
+def distribution():
+    pass
+
+@app.route('/distribution/complete', methods = ['GET', 'POST'])
+def distribution_transform():
+    pass
+
+
+# @app.route('/complete', methods = ['GET', 'POST'])
+# def transform_file():
+#     if request.method == 'POST':
+#         files_removing(uploaded_file_path)
+#         files_removing(gened_file_path)
+#         try:
+#             f = request.files['file']
+#             upload_file_path = uploaded_file_path + secure_filename(f.filename)
+
+#             gen_file_path_1 = gened_file_path + '발주파일.xlsx'
+#             gen_file_path_2 = gened_file_path + '물류파일.xlsx'   # 물류파일 저장할 경로
+#             f.save(upload_file_path)
+
+#             order2order(upload_file_path).to_excel(gen_file_path_1)
+#             distributon_from_orderinfo(upload_file_path).to_excel(gen_file_path_2)
+            
+#             excel2json(gen_file_path_1, gened_file_path + "발주파일.json")
+#             excel2json(gen_file_path_2, gened_file_path + "물류파일.json")
+            
+#             return render_template('complete.html')
+#         except:
+#             return '파일 변환에 실패하였습니다. 다시 시도해주세요.'
 
 if __name__ == "__main__":
     app.run()

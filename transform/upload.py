b4d33fe1401421feae609344250f5c8ab4a48bac
focapa <60702541+focapa@users.noreply.github.com>
2021-08-11 11:32:07 +0900

    Update upload.py

--- transform/upload.py
+++ transform/upload.py
@@ -4,6 +4,7 @@ from lib.brand_domestic import brand2domestic
 from lib.brand_SouthEastAsia import brand2SEA
 from lib.brand_china import brand2china
 from path import UPLOAD_DIR_PATH
+import datetime as dt
 
 bp = Blueprint('upload', __name__, url_prefix='/upload')
 
@@ -20,23 +21,25 @@ def upload_complete():
             # form 데이터 받기 / 파일, 서버선택리스트
             file = request.files['file']
             server_list = request.form.getlist('server')
-            file_name = request.args.get('name', None)
-            file_list = []
+
+            x = dt.datetime.now()
+            file_name = f"{x.year}{x.month}{x.day}{x.hour}{x.minute}{x.second}{x.microsecond}"
+            file_name_list = []
 
             if 'Domestic' in server_list:
                 path_1 = f"{UPLOAD_DIR_PATH}/{file_name}국내서버업로드용.xlsx"
                 brand2domestic(file,path_1)
-                file_list.append(path_1)
+                file_name_list.append(path_1)
             if 'SouthEastAsia' in server_list:
                 path_2 = f"{UPLOAD_DIR_PATH}/{file_name}동남아서버업로드용.xlsx"
                 brand2SEA(file,path_2)
-                file_list.append(path_2)
+                file_name_list.append(path_2)
             if 'China' in server_list:
                 path_3 = f"{UPLOAD_DIR_PATH}/{file_name}중국(위챗)서버업로드용.xlsx"
                 brand2china(file,path_3)
-                file_list.append(path_3)
+                file_name_list.append(path_3)
             
 
-            return render_template('upload/upload_complete.html', server_list=server_list, file_list = file_list)
+            return render_template('upload/upload_complete.html', server_list=server_list, file_list = file_name_list)
         except:
             return '파일 변환에 실패하였습니다. 다시 시도해주세요.'

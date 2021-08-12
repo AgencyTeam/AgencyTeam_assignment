aa4f80c66ce046b6d13d067e3e7a66818d12ec44
mungiyo <wkdtjdxo1234@naver.com>
2021-08-12 22:17:00 +0900

    __init__.py 수정

--- transform/__init__.py
+++ transform/__init__.py
@@ -1,4 +1,5 @@
 import os
+import secrets
 from flask import Flask, send_from_directory
 from path import UPLOAD_DIR_PATH
 from . import main
@@ -13,16 +14,6 @@ def create_app():
     # instance_relative_config=True -> app에게 설정파일은 instance 폴더에 있다. (설정값, DB파일 등)
     app = Flask(__name__, instance_relative_config=True)
 
-    # app의 기본 설정 세팅
-    app.config.from_mapping(
-        # 데이터 보안을 위해 사용. 개발 중에는 편의를 위해 'dev', 실 가동 환경에서는 꼭 랜덤값으로 엎어써야함.
-        SECRET_KEY='dev',
-        # SQlite DB 파일의 경로.
-        # instance 밑에 transform.sqlite 라는 이름.
-        DATABASE=os.path.join(app.instance_path, 'transform.sqlite'),
-        UPLOAD_FOLDER=UPLOAD_DIR_PATH,
-    )
-
     # instance 폴더가 없으면 생성
     try:
         os.makedirs(app.instance_path)
@@ -34,6 +25,29 @@ def create_app():
         os.makedirs(UPLOAD_DIR_PATH)
     except OSError:
         pass
+
+    # app의 기본 설정 세팅
+    if os.environ["FLASK_ENV"] == "development":
+        SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
+        app.config.from_mapping(
+            # 데이터 보안을 위해 사용. 개발 중에는 편의를 위해 'dev', 실 가동 환경에서는 꼭 랜덤값으로 엎어써야함.
+            SECRET_KEY=SECRET_KEY,
+            # SQlite DB 파일의 경로.
+            # instance 밑에 transform.sqlite 라는 이름.
+            DATABASE=os.path.join(app.instance_path, 'transform.sqlite'),
+            UPLOAD_FOLDER=UPLOAD_DIR_PATH,
+        )
+    else:
+        SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_urlsafe(16))
+        app.config.from_mapping(
+            # 데이터 보안을 위해 사용. 개발 중에는 편의를 위해 'dev', 실 가동 환경에서는 꼭 랜덤값으로 엎어써야함.
+            SECRET_KEY=SECRET_KEY,
+            # SQlite DB 파일의 경로.
+            # instance 밑에 transform.sqlite 라는 이름.
+            DATABASE=os.path.join(app.instance_path, 'transform.sqlite'),
+            UPLOAD_FOLDER=UPLOAD_DIR_PATH,
+            DEBUG=False,
+        )
 
     app.register_blueprint(main.bp)
     app.register_blueprint(order.bp)

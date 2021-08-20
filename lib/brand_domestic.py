aa2c3d5e0e712b09bb2a75eefa4c7c26e7e24af4
focapa <60702541+focapa@users.noreply.github.com>
2021-08-20 12:19:45 +0900

    Update brand_domestic.py

--- lib/brand_domestic.py
+++ lib/brand_domestic.py
@@ -1,55 +1,83 @@
 import pandas as pd
 from path import UPLOAD_DOMESTIC_FORM
 from openpyxl import load_workbook 
-
+pd.set_option("display.max_columns",20)
 
 def excel2df(file_path):
     try:
-        df = pd.read_excel(file_path)
+        df = pd.read_excel(file_path,sheet_name="국내서버")
+        df = df.fillna("")
         return df
     except:
         print("엑셀파일을 불러오는데 실패했습니다.")
         return 0
 
-def find_list(jpg_list,name):
-    return ','.join([i for i in jpg_list if i.startswith(name) ])
 
 def generate_df(brand,order_columns):
     # 빈 데이터프레임 선언
     data = pd.DataFrame()
+    # 사용되지 않은 brand 컬럼들 : 상세정보(url), 세탁방법, 소재, 세부사이즈(other),세부사이즈(상의), 세부사이즈(하의),분류,
 
-    # 통합데이터포맷 값 직접사용
-    data["브랜드"] = brand["브랜드명"].str.strip()
-    data["자체 상품코드"] = brand["상품코드"].str[0:50]
+    # 통합데이터포맷 값 사용
     data["상품명"] = brand["제품명"].str[0:100]
-    data["필수옵션명"] = "색상"
-    data["필수옵션값"] = brand["색상"]
-    data["필수옵션가"] = brand["가격"]
-    data["선택옵션명"] = "사이즈"
-    data["선택옵션값"] = brand["사이즈"]
-    data["선택옵션가"] = brand["가격"]
+    data["자체 상품코드"] = brand["상품코드"].str[0:50]
+    # 카테고리ID
+    data["요약설명"] = brand["상품설명"]
+    print(type(brand["재고수량"]))
+    # data["재고수량"] = brand["재고수량"]
+    # data["판매상태"] = data["재고수량"].apply(lambda x: "판매중" if x != 0 else "품절")
+    data["상품상태"] = "신상품"
     data["판매가"] = brand["가격"]
     data["무게"] = brand["무게(g)"]
-    data["제조사"] = brand["제조사"].str.strip()
-    data["원산지"] = brand["원산지"]
-    data["요약설명"] = brand["상품설명"]
-    data["상품 상세정보"] = brand["상세정보(html)"]
-    data["재고수량"] = brand["재고수량"]
-
-    data["판매상태"] = brand["재고수량"].apply(lambda x: "판매중" if x > 0 else "품절")
-    # data["대표 이미지 파일명"] = brand["상품코드"].apply(lambda x : find_list(jpg_list,x))
-
-    # 미입력시 기본값 적용되는 컬럼들.
-    data["상품상태"] = "신상품"
+    # 정가
     data["재고사용"] = "Y"
+    # SKU(재고번호)
+    # (에러) data["대표이미지파일명"] = brand["이미지"]
+    data["상품 상세정보"] = brand["상세정보(html)"]
     data["세금"] = "과세상품"
     data["미성년자 구매"] = "Y"
+    # 개인통관고유부호
+    data["원산지"] = brand["원산지"]
+    data["제조사"] = brand["제조사"].str.strip()
+    data["브랜드"] = brand["브랜드명"].str.strip()
+    # 배송방법, 택배 가능 여부, 퀵서비스 가능 여부, 방문 수령 가능 여부, 배송비 결제 방법, 배송비 유형,'기본배송비','조건부무료-상품판매가합계','무게별 차등 배송비(기본가격)','무게별 차등 배송비(무게)','무게별 차등 배송비(가격)','수량별 차등 배송비(타입)','수량별 차등 배송비(기본 가격)','수량별 차등 배송비(수량)','수량별 차등 배송비(가격)','수량별 차등 배송비(반복수량)','수량별 차등 배송비(반복가격)','구매금액별 차등 배송비(기본 가격)','구매금액 차등 배송비(구매금액)','구매금액별 차등 배송비(가격)',
     data["묶음배송 가능"] = "Y"
     data["별도설치비"] = "N"
     data["지역별 배송비 사용 여부"] = "A"
+    # '지역별 배송비 유형','간편설정(제주도 추가 배송비)','간편설정(도서산간 추가 배송비)','우편번호 등록(시작구간)','우편번호 등록(종료구간)','우편번호 등록(추가배송비)','상품구매시 적립금 지급 값','상품구매시 적립금 지급 단위','할인적용대상','할인금액','할인금액 단위',
     data["옵션형태"] = "조합형"
+    data["필수 옵션명"] = "색상\n사이즈"
+    data["필수 옵션값"] = brand["색상"] + "\n" + brand["사이즈"]
+
+    # data["색상 개수"] = brand["색상"].apply(lambda x : len(x.split(",")))
+    # data["사이즈 개수"]
+    # data["재고수량 리스트"]
+
+    # data["필수 옵션가"]
+    # data["옵션 재고수량"]
+
+    # # len(x.split(",")
+    # data["임시 색상"] = brand["색상"].apply(lambda x : option_split(x) if x != "" else "ONE COLOR")
+    # # data["임시 사이즈"] = brand["사이즈"].apply(lambda x : option_split(x) if x != "" else "ONE SIZE")
+    # # data["필수 옵션값"] = data["임시 색상"] + "\n" + data["임시 사이즈"]
+    # data["필수 옵션값"] = data["임시 색상"] + "\n"
+    # del data["임시 색상"]
+    # # del data["임시 사이즈"]
+    # data["임시 개수"] = data["필수 옵션값"].apply(lambda x : option_count(x))
+
+    
+    print(brand.head(5))
+    print(data.head(5))
+
+    # data["필수 옵션가"] = brand["가격"].astype(str) * data["임시 개수"]
+    # del data["색상 개수"]
+
+
+
+    # '선택 옵션명','선택 옵션값','선택 옵션가','선택 옵션 재고수량','사용자 입력형 옵션','0원 선택옵션 최대 구매수량'
     data["재고소진후주문가능여부"] = "N"
     # data["네이버/다음 쇼핑 노출용 상품명"] = brand["상품명"].str[0:100]
+    # '네이버 쇼핑 이벤트 문구','네이버 쇼핑 카테고리 ID','최소 구매수량','1회 구매시 최대 수량','1인 최대 구매수량'
     data["주문제작상품"] = "N"
     data["병행수입"] = "N"
     data["해외구매대행"] = "N"

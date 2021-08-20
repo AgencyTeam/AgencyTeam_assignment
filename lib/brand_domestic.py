6579636825003c1e0a3ab695a49be09737019d1b
focapa <60702541+focapa@users.noreply.github.com>
2021-08-20 15:50:33 +0900

    Update brand_domestic.py

--- lib/brand_domestic.py
+++ lib/brand_domestic.py
@@ -12,20 +12,56 @@ def excel2df(file_path):
         print("엑셀파일을 불러오는데 실패했습니다.")
         return 0
 
+def stock_sum(x):
+    try:
+        x_list = x.split(',')
+        x_int_list=list(map(int, x_list))
+        return sum(x_int_list)
+    except:
+        return x
+
+def option_price(color, size, price):
+    Ncolor = len(color.split(','))
+    Lcolor = (str(price)+",") * Ncolor
+    Lcolor = Lcolor[:-1]
+    
+    Nsize = len(size.split(','))
+    Lsize = (str(price)+",") * Nsize
+    Lsize = Lsize[:-1]
+    return Lcolor + "\n" + Lsize
+
+def option_stock_num(color, stock):
+    try:
+        x_list = stock.split(',')
+        x_int_list=list(map(int, x_list))
+        Ncolor = len(color.split(','))
+        num = int(len(x_int_list)/Ncolor)
+        i = 0
+        for N in range(Ncolor,0):
+            Lstock = []
+            tmp = x_int_list[i:(i+num-1)]
+            Lstock.append(sum(tmp))
+            i += num
+        Lstock = list(map(str, Lstock))
+        return ','.join(Lstock)
+    except:
+        return stock
+    return
 
 def generate_df(brand,order_columns):
     # 빈 데이터프레임 선언
     data = pd.DataFrame()
-    # 사용되지 않은 brand 컬럼들 : 상세정보(url), 세탁방법, 소재, 세부사이즈(other),세부사이즈(상의), 세부사이즈(하의),분류,
 
-    # 통합데이터포맷 값 사용
+    # -------------------------------------통합데이터포맷 값 사용--------------------------------------------
+    # [사용되지 않은 brand 컬럼들] : 상세정보(url), 세탁방법, 소재, 세부사이즈(other),세부사이즈(상의), 세부사이즈(하의),분류
+    # 중간에 있는 컬럼명들은 서버에만 있는값.
+
     data["상품명"] = brand["제품명"].str[0:100]
     data["자체 상품코드"] = brand["상품코드"].str[0:50]
     # 카테고리ID
     data["요약설명"] = brand["상품설명"]
-    print(type(brand["재고수량"]))
-    # data["재고수량"] = brand["재고수량"]
-    # data["판매상태"] = data["재고수량"].apply(lambda x: "판매중" if x != 0 else "품절")
+    data["재고수량"] = brand["재고수량"].apply(lambda x : stock_sum(x))
+    data["판매상태"] = data["재고수량"].apply(lambda x: "판매중" if x != 0 else "품절")
     data["상품상태"] = "신상품"
     data["판매가"] = brand["가격"]
     data["무게"] = brand["무게(g)"]
@@ -47,37 +83,18 @@ def generate_df(brand,order_columns):
     # '지역별 배송비 유형','간편설정(제주도 추가 배송비)','간편설정(도서산간 추가 배송비)','우편번호 등록(시작구간)','우편번호 등록(종료구간)','우편번호 등록(추가배송비)','상품구매시 적립금 지급 값','상품구매시 적립금 지급 단위','할인적용대상','할인금액','할인금액 단위',
     data["옵션형태"] = "조합형"
     data["필수 옵션명"] = "색상\n사이즈"
+    brand["색상"] = brand["색상"].apply(lambda x : x if x != "" else "ONE COLOR")
+    brand["사이즈"] = brand["사이즈"].apply(lambda x : x if x != "" else "ONE SIZE")
     data["필수 옵션값"] = brand["색상"] + "\n" + brand["사이즈"]
-
-    # data["색상 개수"] = brand["색상"].apply(lambda x : len(x.split(",")))
-    # data["사이즈 개수"]
-    # data["재고수량 리스트"]
-
-    # data["필수 옵션가"]
-    # data["옵션 재고수량"]
-
-    # # len(x.split(",")
-    # data["임시 색상"] = brand["색상"].apply(lambda x : option_split(x) if x != "" else "ONE COLOR")
-    # # data["임시 사이즈"] = brand["사이즈"].apply(lambda x : option_split(x) if x != "" else "ONE SIZE")
-    # # data["필수 옵션값"] = data["임시 색상"] + "\n" + data["임시 사이즈"]
-    # data["필수 옵션값"] = data["임시 색상"] + "\n"
-    # del data["임시 색상"]
-    # # del data["임시 사이즈"]
-    # data["임시 개수"] = data["필수 옵션값"].apply(lambda x : option_count(x))
-
+    data["필수 옵션가"] = brand.apply(lambda x:option_price(x["색상"], x["사이즈"],x["가격"]), axis=1)
+    data["옵션 재고수량"] = brand.apply(lambda x:option_stock_num(x["색상"],x["재고수량"]), axis=1)
     
     print(brand.head(5))
     print(data.head(5))
 
-    # data["필수 옵션가"] = brand["가격"].astype(str) * data["임시 개수"]
-    # del data["색상 개수"]
-
-
-
     # '선택 옵션명','선택 옵션값','선택 옵션가','선택 옵션 재고수량','사용자 입력형 옵션','0원 선택옵션 최대 구매수량'
     data["재고소진후주문가능여부"] = "N"
-    # data["네이버/다음 쇼핑 노출용 상품명"] = brand["상품명"].str[0:100]
-    # '네이버 쇼핑 이벤트 문구','네이버 쇼핑 카테고리 ID','최소 구매수량','1회 구매시 최대 수량','1인 최대 구매수량'
+    # "네이버/다음 쇼핑 노출용 상품명", '네이버 쇼핑 이벤트 문구','네이버 쇼핑 카테고리 ID','최소 구매수량','1회 구매시 최대 수량','1인 최대 구매수량'
     data["주문제작상품"] = "N"
     data["병행수입"] = "N"
     data["해외구매대행"] = "N"
@@ -139,5 +156,4 @@ def brand2domestic(file_path,upload_path):
     brand_df.drop([brand_df.index[0],brand_df.index[1]],inplace=True)
 
     domestic_df = generate_df(brand_df, order_columns)
-    # print(domestic_df.head(5))
     df2excel(domestic_df,UPLOAD_DOMESTIC_FORM,upload_path)
